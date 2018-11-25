1d0ac501dc174403ea07c498563de13236780f56
kjefferson1992 <40824814+kjefferson1992@users.noreply.github.com>
2018-11-25 10:06:21 +0100

    Add files via upload

---
+++ PodcastsCourseLBTA4/PodcastsCourseLBTA/MainTabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  MainTabBarController.swift
+//  PodcastsCourseLBTA
+//
+//  Created by Brian Voong on 2/13/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    //MARK:- Setup Functions
+    
+    func setupViewControllers() {
+        viewControllers = [
+            generateNavigationController(for: PodcastsSearchController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- Helper Functions
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+//        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+    
+    
+    
+    
+    
+    
+}
