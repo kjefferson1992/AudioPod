28cba40fa172607886a358611b8c75166eeda453
kjefferson1992 <40824814+kjefferson1992@users.noreply.github.com>
2018-11-25 10:10:00 +0100

    Add files via upload

---
+++ PodcastsCourseLBTA5/PodcastsCourseLBTA/Controllers/PodcastsSearchController.swift
@@ -0,0 +1,107 @@
+//
+//  PodcastsSearchController.swift
+//  PodcastsCourseLBTA
+//
+//  Created by Brian Voong on 2/14/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
+    
+    var podcasts = [
+        Podcast(trackName: "Lets Build That App", artistName: "Brian Voong"),
+        Podcast(trackName: "Some Podcast", artistName: "Some Author"),
+    ]
+    
+    let cellId = "cellId"
+    
+    // lets implement a UISearchController
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSearchBar()
+        setupTableView()
+    }
+    
+    //MARK:- Setup Work
+    
+    fileprivate func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        print(searchText)
+        // later implement Alamofire to search iTunes API
+        
+//        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        let url = "https://itunes.apple.com/search"
+        let parameters = ["term": searchText, "media": "podcast"]
+        
+        Alamofire.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+            
+            if let err = dataResponse.error {
+                print("Failed to contact yahoo", err)
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResults.self, from: data)
+                self.podcasts = searchResult.results
+                self.tableView.reloadData()
+            } catch let decodeErr {
+                print("Failed to decode:", decodeErr)
+            }
+            
+        }
+    }
+    
+    struct SearchResults: Decodable {
+        let resultCount: Int
+        let results: [Podcast]
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    //MARK:- UITableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let podcast = self.podcasts[indexPath.row]
+        cell.textLabel?.text = "\(podcast.trackName ?? "")\n\(podcast.artistName ?? "")"
+        cell.textLabel?.numberOfLines = -1
+        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PodcastsCourseLBTA5/PodcastsCourseLBTA/Models/Podcast.swift
@@ -0,0 +1,15 @@
+//
+//  Podcast.swift
+//  PodcastsCourseLBTA
+//
+//  Created by Brian Voong on 2/14/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast: Decodable {
+    var trackName: String?
+    var artistName: String?
+}
+
