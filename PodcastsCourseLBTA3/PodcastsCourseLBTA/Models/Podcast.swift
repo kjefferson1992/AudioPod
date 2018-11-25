f0c8f12d8a2a52eb87d3d813ea0911aff1efe928
kjefferson1992 <40824814+kjefferson1992@users.noreply.github.com>
2018-11-25 10:04:00 +0100

    Add files via upload

---
+++ PodcastsCourseLBTA3/PodcastsCourseLBTA/Models/Podcast.swift
@@ -0,0 +1,15 @@
+//
+//  Podcast.swift
+//  PodcastsCourseLBTA
+//
+//  Created by Brian Voong on 2/14/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast {
+    let name: String
+    let artistName: String
+}
+
